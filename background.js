d8907bbfae8c555183408e48d20cbbb279f815ab
Eric Ngoiya <115367894+ngoiyaeric@users.noreply.github.com>
2024-05-20 22:20:15 +0300

    .

---
+++ background.js
@@ -0,0 +1,161 @@
+// Initialize chat history
+let chatHistory;
+
+// Listen for when the extension is installed
+chrome.runtime.onInstalled.addListener(function () {
+    // Set default API model
+    let defaultModel = "gpt-4o";
+    chrome.storage.local.set({ apiModel: defaultModel });
+
+    // Set empty chat history
+    chrome.storage.local.set({ chatHistory: [] });
+
+    // Open the options page
+    chrome.runtime.openOptionsPage();
+});
+
+// Listen for messages from the popup script
+chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
+
+    if (message.userInput) {
+
+        // Get the API key from local storage
+        const { apiKey } = await getStorageData(["apiKey"]);
+        // Get the model from local storage
+        const { apiModel } = await getStorageData(["apiModel"]);
+
+        // get the chat history from local storage
+        const result = await getStorageData(["chatHistory"]);
+
+        if (!result.chatHistory || result.chatHistory.length === 0) {
+            chatHistory = [
+                { role: "system", content: "I'm your helpful chat bot! I provide helpful and concise answers." },
+            ];
+        } else {
+            chatHistory = result.chatHistory;
+        }
+
+        // save user's message to message array
+        chatHistory.push({ role: "user", content: message.userInput });
+
+        if (apiModel === "dall-e-3") {
+            // Send the user's message to the OpenAI API
+            const response = await fetchImage(message.userInput, apiKey, apiModel);
+
+            if (response && response.data && response.data.length > 0) {
+                // Get the image URL
+                const imageUrl = response.data[0].url;
+
+                // Add the assistant's response to the message array
+                chatHistory.push({ role: "assistant", content: imageUrl });
+
+                // save message array to local storage
+                chrome.storage.local.set({ chatHistory: chatHistory });
+
+                // Send the image URL to the popup script
+                chrome.runtime.sendMessage({ imageUrl: imageUrl });
+
+                console.log("Sent image URL to popup:", imageUrl);
+            }
+            return true; // Enable response callback
+        } else {
+            // Send the user's message to the OpenAI API
+            const response = await fetchChatCompletion(chatHistory, apiKey, apiModel);
+
+            if (response && response.choices && response.choices.length > 0) {
+
+                // Get the assistant's response
+                const assistantResponse = response.choices[0].message.content;
+
+                // Add the assistant's response to the message array
+                chatHistory.push({ role: "assistant", content: assistantResponse });
+
+                // save message array to local storage
+                chrome.storage.local.set({ chatHistory: chatHistory });
+
+                // Send the assistant's response to the popup script
+                chrome.runtime.sendMessage({ answer: assistantResponse });
+
+                console.log("Sent response to popup:", assistantResponse);
+            }
+            return true; // Enable response callback
+        }
+    }
+
+    return true; // Enable response callback
+});
+
+// Fetch data from the OpenAI Chat Completion API
+async function fetchChatCompletion(messages, apiKey, apiModel) {
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            },
+            body: JSON.stringify({
+                "messages": messages,
+                "model": apiModel,
+            })
+        });
+
+        if (!response.ok) {
+            if (response.status === 401) {
+                // Unauthorized - Incorrect API key
+                throw new Error("Looks like your API key is incorrect. Please check your API key and try again.");
+            } else {
+                throw new Error(`Failed to fetch. Status code: ${response.status}`);
+            }
+        }
+
+        return await response.json();
+    } catch (error) {
+        // Send a response to the popup script
+        chrome.runtime.sendMessage({ error: error.message });
+
+        console.error(error);
+    }
+}
+
+// Fetch Image from the OpenAI DALL-E API
+async function fetchImage(prompt, apiKey, apiModel) {
+    try {
+        const response = await fetch('https://api.openai.com/v1/images/generations', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            },
+            body: JSON.stringify({
+                "prompt": prompt,
+                "model": apiModel,
+                "n": 1,
+                "size": "1024x1024",
+            })
+        });
+
+        if (!response.ok) {
+            if (response.status === 401) {
+                // Unauthorized - Incorrect API key
+                throw new Error("Looks like your API key is incorrect. Please check your API key and try again.");
+            } else {
+                throw new Error(`Failed to fetch. Status code: ${response.status}`);
+            }
+        }
+
+        return await response.json();
+    } catch (error) {
+        // Send a response to the popup script
+        chrome.runtime.sendMessage({ error: error.message });
+
+        console.error(error);
+    }
+}
+
+// Get data from local storage
+function getStorageData(keys) {
+    return new Promise((resolve) => {
+        chrome.storage.local.get(keys, (result) => resolve(result));
+    });
+}
---
+++ popup/popup.js
@@ -0,0 +1,381 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const chatMessages = document.getElementById('chat-messages');
+    const userInput = document.getElementById('user-input');
+    const sendBtn = document.getElementById('send-btn');
+    const clearChatBtn = document.getElementById('clear-chat-btn');
+
+    // If the user has not entered an API key, open the options page
+    chrome.storage.local.get('apiKey', ({ apiKey }) => {
+        if (!apiKey || apiKey.length < 10) {
+            chrome.runtime.openOptionsPage();
+        }
+    });
+
+    // Fetch chat history from local storage and display it
+    chrome.storage.local.get(['chatHistory'], function (result) {
+        const chatHistory = result.chatHistory || [];
+
+        if (chatHistory.length > 0) {
+            // display the chat history
+            displayMessages(chatHistory);
+
+            // scroll to bottom of chat-messages div    
+            chatMessages.scrollTop = chatMessages.scrollHeight;
+        } else {
+            // show a image of the icon and a text with "how can I help you?"
+            displayAssistanInfo();
+        }
+
+        checkClearChatBtn();
+    });
+
+    // focus on the input field
+    userInput.focus();
+
+    // disable the send button by default
+    sendBtn.disabled = true;
+
+    // disable the send button if the input field is empty
+    userInput.addEventListener('keyup', function () {
+        const userMessage = userInput.value.trim();
+        sendBtn.disabled = userMessage === '';
+    });
+
+    // If the user presses enter, click the send button
+    userInput.addEventListener('keyup', function (event) {
+        if (event.code === 'Enter' && !event.shiftKey) {
+            event.preventDefault();
+            sendBtn.click();
+        }
+    });
+
+    // Auto-resize the input field based on the content
+    userInput.addEventListener('input', function () {
+        this.style.height = 'auto'; // Reset the height
+        // Set the height of the input field based on the content
+        this.style.height = Math.min(this.scrollHeight, 100) + 'px';
+        // Enable scrolling if the content is too long
+        this.style.overflowY = this.scrollHeight > 100 ? 'scroll' : 'auto';
+    });
+
+    // Send user's input to background script when the send button is clicked
+    sendBtn.addEventListener('click', function () {
+        const userMessage = userInput.value.trim();
+        if (userMessage !== '') {
+            sendMessage(userMessage);
+            userInput.value = ''; // Clear the input field
+
+            // disable the send button
+            sendBtn.disabled = true;
+            // remove the icon from the send button and add the loading indicator
+            sendBtn.innerHTML = '<i class="fa fa-spinner fa-pulse"></i>';
+
+            // disable input field while the assistant is typing
+            userInput.disabled = true;
+            userInput.style.height = 'auto'; // Reset the height
+
+            // scroll to bottom of chat-messages div
+            chatMessages.scrollTop = chatMessages.scrollHeight;
+        }
+    });
+
+    // Listen for messages from the background script
+    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+        if (message.answer || message.imageUrl) {
+            // Display the assistant's response
+            displayMessage('assistant', message.answer || message.imageUrl);
+        } else if (message.error) {
+            // Display the error message
+            displayMessage('system', message.error);
+        } else if (message.imageUrl) {
+            // Display the image in the chat
+            displayMessage('assistant', message.imageUrl);
+        }
+
+        // Enable the send button again
+        sendBtn.disabled = false;
+        // Add the send icon to the send button and remove the loading indicator
+        sendBtn.innerHTML = '<i class="fa fa-paper-plane"></i>';
+
+        // Enable the input field again
+        userInput.disabled = false;
+    });
+
+    // Function to send user's input to the background script and display it in the chat
+    function sendMessage(userMessage) {
+        const message = { userInput: userMessage };
+        // Send the user's message to the background script
+        chrome.runtime.sendMessage(message);
+        if (document.getElementById('assistant-info-wrapper')) {
+            hideAssistanInfo();
+        }
+        // Display the user's message in the chat
+        displayMessage('user', userMessage);
+    }
+
+    // Function to display messages in the chat
+    function displayMessage(role, content) {
+        const messageElement = document.createElement('div');
+        // add id to the message element
+        messageElement.classList.add('message');
+        messageElement.classList.add(role);
+
+        // check of message starts with a dall-e image URL
+        if (content.startsWith('https://oaidalleapiprodscus.blob.core.windows.net/')) {
+            const imageElement = document.createElement('img');
+            imageElement.src = content;
+            messageElement.appendChild(imageElement);
+
+            // add a download button to the message if it's from the assistant
+            if (role === 'assistant') {
+                // create container for the action buttons
+                const actionBtns = document.createElement('div');
+                actionBtns.className = 'action-btns';
+
+                // add the action buttons to the message
+                messageElement.appendChild(actionBtns);
+
+                const downloadIcon = document.createElement('i');
+                downloadIcon.className = 'fa fa-download download-btn';
+                downloadIcon.title = 'Download image';
+                downloadIcon.addEventListener('click', function () {
+                    // download image to the user's device
+                    chrome.downloads.download({
+                        url: content,
+                        filename: 'dall-e-image.png',
+                        saveAs: false
+                    })
+                        .then(() => {
+                            // Change the icon to a check
+                            downloadIcon.className = 'fa fa-check action-btn';
+
+                            // Revert to the default icon after 2 seconds
+                            setTimeout(() => {
+                                downloadIcon.className = 'fa fa-download action-btn';
+                            }, 2000);
+                        })
+                        // Display an x icon if the copy operation fails
+                        .catch(() => {
+                            downloadIcon.className = 'fa fa-times action-btn';
+
+                            // Revert to the default icon after 2 seconds
+                            setTimeout(() => {
+                                downloadIcon.className = 'fa fa-download action-btn';
+                            }, 2000);
+                        });
+
+                });
+
+                actionBtns.appendChild(downloadIcon);
+            }
+
+        } else { // if it's not an image, it's a text message
+            // format the message content
+            content = formatMessageContent(content);
+
+            // add the message content to the message element
+            messageElement.innerHTML = content;
+
+            // add a copy button to the message if it's from the assistant
+            if (role === 'assistant') {
+                // create container for the action buttons
+                const actionBtns = document.createElement('div');
+                actionBtns.className = 'action-btns';
+
+                // add the action buttons to the message
+                messageElement.appendChild(actionBtns);
+
+                const copyIcon = document.createElement('i');
+                copyIcon.className = 'fa fa-copy action-btn';
+                copyIcon.title = 'Copy to clipboard';
+                copyIcon.addEventListener('click', function () {
+                    // Copy the message to the clipboard
+                    navigator.clipboard.writeText(content)
+                        .then(() => {
+                            // Change the icon to a check
+                            copyIcon.className = 'fa fa-check action-btn';
+
+                            // Revert to the default icon after 2 seconds
+                            setTimeout(() => {
+                                copyIcon.className = 'fa fa-copy action-btn';
+                            }, 2000);
+                        })
+                        // Display an x icon if the copy operation fails
+                        .catch(() => {
+                            copyIcon.className = 'fa fa-times action-btn';
+
+                            // Revert to the default icon after 2 seconds
+                            setTimeout(() => {
+                                copyIcon.className = 'fa fa-copy action-btn';
+                            }, 2000);
+                        });
+                });
+
+                actionBtns.appendChild(copyIcon);
+            }
+
+        }
+        chatMessages.appendChild(messageElement);
+
+        // enable the clear chat button
+        checkClearChatBtn();
+
+        // scroll to the displayed message in the chat-messages div
+        messageElement.scrollIntoView();
+    }
+
+    function formatMessageContent(text) {
+        return text.replace(/```(\w+)([\s\S]*?)```/g, function (match, lang, code) {
+            // Create a code element
+            var codeElement = document.createElement('code');
+            // remove the first line break from the code
+            code = code.replace(/^\n/, '');
+
+            // Add the code to the code element
+            codeElement.innerText = code;
+
+            // Create a container for the code element
+            var codeContainer = document.createElement('div');
+            codeContainer.appendChild(codeElement);
+
+            // Set the class of the container based on the language (optional)
+            codeContainer.className = 'code-block';
+
+            // Return the HTML content with the replaced code
+            return codeContainer.outerHTML;
+        });
+    }
+
+    // Function to display an array of messages
+    function displayMessages(messages) {
+        for (const message of messages) {
+            if (message.role !== 'system') {
+                displayMessage(message.role, message.content);
+            }
+        }
+    }
+
+    // fucntion to check if the clear chat button should be enabled or disabled
+    function checkClearChatBtn() {
+        chrome.storage.local.get(['chatHistory'], function (result) {
+            const chatHistory = result.chatHistory || [];
+            if (chatHistory.length > 0) {
+                clearChatBtn.disabled = false;
+            } else {
+                clearChatBtn.disabled = true;
+            }
+        });
+    }
+
+    // Clear the chat history when the clear chat button is clicked
+    clearChatBtn.addEventListener('click', function () {
+        // Display a confirmation popup
+        const isConfirmed = window.confirm('Are you sure you want to clear the chat history?');
+
+        // If the user confirms, clear the chat history
+        if (isConfirmed) {
+            chrome.storage.local.set({ chatHistory: [] }, function () {
+                console.log('Chat history cleared');
+                chatMessages.innerHTML = '';
+                sendBtn.disabled = true;
+                checkClearChatBtn();
+                displayAssistanInfo();
+            });
+        }
+    });
+
+    // Settings button click event
+    document.getElementById('settings-btn').addEventListener('click', function () {
+        chrome.runtime.openOptionsPage();
+    });
+
+    // Open the dropdown when the button is clicked
+    document.getElementById("model-dropdown-btn").addEventListener("click", function () {
+        var dropdownContent = document.getElementById("model-dropdown-content");
+
+        // Toggle the display property
+        dropdownContent.style.display = (dropdownContent.style.display === "flex") ? "none" : "flex";
+
+        // Add active class to the button if the dropdown is open
+        document.getElementById("model-dropdown-btn").classList.toggle("active", dropdownContent.style.display === "flex");
+    });
+
+
+    // Close the dropdown if the user clicks outside of it
+    window.addEventListener("click", function (event) {
+        if (!event.target.matches('#model-dropdown-btn')) {
+            var dropdownContent = document.getElementById("model-dropdown-content");
+            if (dropdownContent.style.display === "flex") {
+                dropdownContent.style.display = "none";
+            }
+            // remove active class from the button if the dropdown is closed
+            document.getElementById("model-dropdown-btn").classList.remove("active");
+        }
+    });
+
+    // Handle button clicks in the dropdown
+    var dropdownButtons = document.querySelectorAll(".model-dropdown-btn");
+    dropdownButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            // Get the ID of the clicked button
+            var buttonId = button.id;
+
+            // Set the localStorage value
+            chrome.storage.local.set({ apiModel: buttonId });
+
+            // Update the text on the main button
+            document.getElementById("model-dropdown-btn-text").innerText = button.innerText;
+
+            // Set active model
+            setActiveModel(buttonId);
+        });
+    });
+
+    // Function to set the active model in the dropdown
+    function setActiveModel(model) {
+        // add active class to the button and remove it from the other buttons
+        dropdownButtons.forEach(function (button) {
+            button.classList.remove("active");
+        });
+        document.getElementById(model).classList.add("active");
+    }
+
+    // Set the active model when the popup is opened
+    chrome.storage.local.get(['apiModel'], function (result) {
+        if (result.apiModel) {
+            // Update the text on the main button
+            document.getElementById("model-dropdown-btn-text").innerText = document.getElementById(result.apiModel).innerText;
+            // Set active model in the dropdown
+            setActiveModel(result.apiModel);
+        }
+    });
+
+    function displayAssistanInfo() {
+        // Create a div element for the message
+        const messageElement = document.createElement('div');
+        messageElement.id = 'assistant-info-wrapper';
+
+        // Create an img element for the icon
+        const icon = document.createElement('img');
+        icon.src = '/assets/icons/icon-128.png';
+        icon.alt = 'Assistant icon';
+        icon.className = 'assistant-info-icon';
+        messageElement.appendChild(icon);
+
+        // Create a p element for the text
+        const text = document.createElement('p');
+        text.innerText = 'How can I help you?';
+        text.className = 'assistant-info-text';
+        messageElement.appendChild(text);
+
+        // Append the message element to the chatMessages container
+        chatMessages.appendChild(messageElement);
+    }
+
+    function hideAssistanInfo() {
+        const assistantInfo = document.getElementById('assistant-info-wrapper');
+        if (assistantInfo) {
+            assistantInfo.remove();
+        }
+    }
+});
